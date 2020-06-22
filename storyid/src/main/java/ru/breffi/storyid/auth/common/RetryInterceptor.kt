c7a16440cff64c91a26f63060f6e220c19608874
Sergey Evstyunin <evstyunin@moqod.com>
2020-06-22 10:18:36 +0400

    Implement retry interceptor to repeat request in case of SocketTimeoutException, all 5xx errors and some of 4xx errors, set client timeouts to 5 seconds

--- storyid/src/main/java/ru/breffi/storyid/auth/common/RetryInterceptor.kt
+++ storyid/src/main/java/ru/breffi/storyid/auth/common/RetryInterceptor.kt
@@ -1,21 +1,27 @@
 package ru.breffi.storyid.auth.common
 
-import okhttp3.*
+import okhttp3.Interceptor
+import okhttp3.Response
 import java.io.IOException
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
+import java.net.SocketTimeoutException
 
-internal class RetryInterceptor(private val retryCount: Int = 3) : Interceptor {
+class RetryInterceptor(private val retryCount: Int = 3) : Interceptor {
+
+    private val retryCodes = (500..599) + 402 + (405..499)
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
-        var response = chain.proceed(request)
+        var response: Response?
         var retry = 0
-        while (!response.isSuccessful && response.code() in 500..599 && retry < retryCount) {
-            response = chain.proceed(request)
-            retry++
-        }
-        return response
+        do {
+            try {
+                response = chain.proceed(request)
+            } catch (e: SocketTimeoutException) {
+                response = null
+                e.printStackTrace()
+            }
+        } while ((response == null || !response.isSuccessful && response.code() in retryCodes) && retry++ < retryCount)
+        return response ?: throw SocketTimeoutException()
     }
 }
