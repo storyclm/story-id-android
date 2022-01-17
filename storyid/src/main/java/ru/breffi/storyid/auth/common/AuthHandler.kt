a78afe58dc29e17f2e74c4e03031b22796e29416
Sergey Evstyunin <sergei.evstyunin@rightperception.company>
2022-01-17 14:04:20 +0400

    Refactor auth logic, add RetryInterceptor to internal client of Authentication, implement AuthLostListener interface

--- storyid/src/main/java/ru/breffi/storyid/auth/common/AuthHandler.kt
+++ storyid/src/main/java/ru/breffi/storyid/auth/common/AuthHandler.kt
@@ -4,7 +4,14 @@ import ru.breffi.storyid.auth.common.model.IdValueResult
 
 interface AuthHandler {
 
+    interface AuthLostListener {
+
+        fun onAuthLost()
+    }
+
     fun userExists(username: String): IdValueResult<Boolean>
+
+    fun setAuthLostListener(listener: AuthLostListener?)
 
     fun logout()
 }
--- storyid/src/main/java/ru/breffi/storyid/auth/common/AuthInterceptor.kt
+++ storyid/src/main/java/ru/breffi/storyid/auth/common/AuthInterceptor.kt
@@ -1,6 +1,7 @@
 package ru.breffi.storyid.auth.common
 
 import okhttp3.*
+import ru.breffi.storyid.auth.common.model.IdException
 import java.io.IOException
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
@@ -15,20 +16,19 @@ internal class AuthInterceptor(private val tokenHandler: TokenHandler) : Interce
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        tokenHandler.getAuthHeaderValue()
-            ?.let { authHeader ->
-                val request = chain.request()
-                    .newBuilder()
-                    .header(AUTH_HEADER_NAME, authHeader)
-                    .build()
-                return chain.proceed(request)
-            } ?: throw IOException()
+        getAuthHeader()?.let { authHeader ->
+            val request = chain.request()
+                .newBuilder()
+                .header(AUTH_HEADER_NAME, authHeader)
+                .build()
+            return chain.proceed(request)
+        } ?: throw IdException(code = 401, message = "Missing authentication data")
     }
 
     @Throws(IOException::class)
     override fun authenticate(route: Route?, response: Response): Request? {
-        return if (response.code() == 401 && tokenHandler.isAccessTokenExpired()) {
-            getRefreshedAuthHeader()
+        return if (response.code() == 401) {
+            getAuthHeader()
                 ?.let { authHeader ->
                     response.request()
                         .newBuilder()
@@ -40,16 +40,12 @@ internal class AuthInterceptor(private val tokenHandler: TokenHandler) : Interce
         }
     }
 
-    private fun getRefreshedAuthHeader() : String? {
-        if (lock.isLocked) {
-            lock.withLock {
-                return tokenHandler.getAuthHeaderValue()
-            }
-        } else {
-            lock.withLock {
+    private fun getAuthHeader(): String? {
+        return lock.withLock {
+            if (tokenHandler.isAccessTokenExpired()) {
                 tokenHandler.refreshAccessToken()
-                return tokenHandler.getAuthHeaderValue()
             }
+            tokenHandler.getAuthHeaderValue()
         }
     }
 }
